62114bf022e75448c7569720eeb86ac506b6f0ac
Marko Tasic <mtasic85@gmail.com>
2023-08-20 17:03:34 +0200

    bg cache prompt

--- README.md
+++ README.md
@@ -1,50 +1,69 @@
 # python-llama-cpp-http
 
-Python llama.cpp HTTP Server and LangChain LLM Client
+Python **llama.cpp** HTTP Server and **LangChain** LLM Client.
 
-Assumption is that **GPU driver** and **OpenCL** library is installed.
+Assumption is that **GPU driver**, and **OpenCL** / **CUDA** libraries are installed.
 
-Make sure you follow instructions of **llama.cpp** for **CLBlast** from https://github.com/ggerganov/llama.cpp .
+Make sure you follow instructions of **llama.cpp** below for
+OpenCL for AMDGPU/NVIDIA **CLBlast**,
+or CUDA for NVIDIA **cuBLIS**
+from https://github.com/ggerganov/llama.cpp .
 
-Download https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/blob/main/llama-2-7b-chat.ggmlv3.q2_K.bin .
+Download https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/blob/main/llama-2-7b-chat.ggmlv3.q2_K.bin
+to local directory `models`.
 
 ## Setup
 
 ### Arch/Manjaro
 
 ```bash
-sudo pacman -Sy base-devel clang cmake ninja git python jq
-
-# AUR
-yay -S clblast-git
+sudo pacman -Sy base-devel clang cmake ninja git python jq blas-openblas sqlite
+yay -Sy clblast-git
 ```
 
 ### Ubuntu/Debian
 
 ```bash
-sudo apt install build-essential python3-dev python3-venv libffi-dev libssl-dev clang cmake ninja-build git jq
+sudo apt install build-essential python3-dev python3-venv libffi-dev libssl-dev clang cmake ninja-build git jq libopenblas-dev libsqlite-dev
 ```
 
-### Build llama.cpp with CLBlast
+### Required for CPU: Build llama.cpp
 
 ```bash
-python -m venv ~/venv
+git clone https://github.com/ggerganov/llama.cpp llama.cpp
+cd llama.cpp
+CC=clang CXX=clang++ make -j
+```
+
+### Optional for AMDGPU or NVIDIA: Build llama.cpp with CLBlast
+
+```bash
+git clone https://github.com/ggerganov/llama.cpp llama.cpp-clblast
+cd llama.cpp-clblast
+CC=clang CXX=clang++ make LLAMA_CLBLAST=1 -j
+```
+
+### Optional for NVIDIA: Build llama.cpp with cuBLIS
+
+```bash
+git clone https://github.com/ggerganov/llama.cpp llama.cpp-cublis
+cd llama.cpp-cublis
+CC=clang CXX=clang++ make LLAMA_CUBLAS=1 -j
+```
+
+### Setup python venv
+
+```bash
+python -m venv venv
 source venv/bin/activate
 python -m ensurepip --upgrade
-pip install -r requirements.txt
-```
-
-```bash
-git clone https://github.com/ggerganov/llama.cpp
-cd llama.cpp
-make LLAMA_CLBLAST=1
-cd ..
+pip install -U .
 ```
 
 ### Run
 ```bash
 source venv/bin/activate
-python -B server.py
+python -m llama_cpp_http.server --backend cpu --models-path ./models --llama-cpp-path ./llama.cpp
 ```
 
 ### Run Client Examples
--- llama_cpp_http/server.py
+++ llama_cpp_http/server.py
@@ -11,51 +11,123 @@ import aiohttp
 from aiohttp import web
 from aiohttp.web import middleware
 from async_timeout import timeout
+from pony import orm
 
 #
 # env
 #
-HOST = os.getenv('HOST') or '0.0.0.0'
-PORT = int(os.getenv('PORT') or 5000)
-TIMEOUT = float(os.getenv('TIMEOUT') or 300.0)
-PLATFORM = int(os.getenv('PLATFORM') or 0)
-MODELS_PATH = os.getenv('MODELS_PATH', './models')
-LLAMA_CPP_PATH = os.getenv('LLAMA_CPP_PATH', './llama.cpp')
-
 parser = argparse.ArgumentParser(prog='server', description='Python llama.cpp HTTP Server')
-parser.add_argument('--host', help='http server host', default=HOST)
-parser.add_argument('--port', help='http server port', default=PORT, type=int)
-parser.add_argument('--timeout', help='llama.cpp timeout in seconds', default=TIMEOUT, type=float)
-parser.add_argument('--platform', help='pyopencl platform number', default=PLATFORM, type=int)
-parser.add_argument('--models-path', help='models direcory path', default=MODELS_PATH)
-parser.add_argument('--llama-cpp-path', help='llama.cpp direcory path', default=LLAMA_CPP_PATH)
+parser.add_argument('--host', help='http server host', default='0.0.0.0')
+parser.add_argument('--port', help='http server port', default=5000, type=int)
+parser.add_argument('--timeout', help='llama.cpp timeout in seconds', default=300.0, type=float)
+parser.add_argument('--backend', help='llama.cpp execution backend', default='cpu', type=str, choices=['cpu', 'clblast', 'cublis'])
+parser.add_argument('--models-path', help='models directory path', default='~/models')
+parser.add_argument('--llama-cpp-path', help='llama.cpp directory path', default='~/llama.cpp')
+parser.add_argument('--allow-bg-cache-prompt', help='allow background prompting and caching for same prompt', type=bool, default=False)
+parser.add_argument('--bg-db-prompt', help='database path for background caching of prompts', type=str, default='~/models/llama_cpp_bg_cache_prompt.sqlite')
+parser.add_argument('--bg-n-tasks-per-prompt', help='number of background tasks for same prompt', type=int, default=3)
+parser.add_argument('--bg-n-results-per-prompt', help='return last N results for same prompt', type=int, default=10)
+parser.add_argument('--bg-llama-cpp-path', help='llama.cpp directory path used for background tasks', default='~/llama.cpp')
 cli_args = parser.parse_args()
 
 HOST = cli_args.host
 PORT = cli_args.port
 TIMEOUT = cli_args.timeout
-PLATFORM = cli_args.platform
+BACKEND = cli_args.backend
 MODELS_PATH = cli_args.models_path
 LLAMA_CPP_PATH = cli_args.llama_cpp_path
+ALLOW_BG_CACHE_PROMPT = cli_args.allow_bg_cache_prompt
+BG_DB_PROMPT = cli_args.bg_db_prompt
+BG_N_TASKS_PER_PROMPT = cli_args.bg_n_tasks_per_prompt
+BG_N_RESULTS_PER_PROMPT = cli_args.bg_n_results_per_prompt
+BG_LLAMA_CPP_PATH = cli_args.bg_llama_cpp_path
+BG_LLAMA_CPP_PATH = cli_args.bg_llama_cpp_path
 
-cl_devices = []
-cl_devices_locks = []
+devices = []
+devices_locks = []
 task_queue = set()
+bg_task_queue = asyncio.Queue()
 
-def init_cl_devices():
-    global cl_devices
-    global cl_devices_locks
+def init_devices():
+    global devices
+    global devices_locks
     
-    for pi, p in enumerate(cl.get_platforms()):
-        for di, d in enumerate(p.get_devices()):
-            n = (pi, di, p, d)
-            cl_devices.append(n)
+    if BACKEND == 'cpu':
+        # allow only ONE "device" concurrently
+        n = (-1, -1, None, None)
+        devices.append(n)
+    elif BACKEND in ('clblast', 'cublis'):
+        # number of devices depend on how many OpenCL finds
+        # this also applies to NVIDIA cuBLIS / cuda devices
+        for pi, p in enumerate(cl.get_platforms()):
+            for di, d in enumerate(p.get_devices()):
+                n = (pi, di, p, d)
+                devices.append(n)
+    else:
+        raise ValueError(BACKEND)
 
-    for n in cl_devices:
-        cl_devices_locks.append((n, asyncio.Lock()))
+    # create async looks, later used for async subprocesses
+    for n in devices:
+        devices_locks.append((n, asyncio.Lock()))
 
-    print('cl_devices_locks:')
-    pprint(cl_devices_locks)
+    print('devices_locks:')
+    pprint(devices_locks)
+
+#
+# background prompting and caching
+#
+if ALLOW_BG_CACHE_PROMPT:
+    db = orm.Database()
+
+    class PromptOutputInfo(db.Entity):
+        id = orm.PrimaryKey(str)
+        prompt_hash = orm.Required(str, index=True)
+        prompt = orm.Required(str)
+        output_hash = orm.Required(str)
+        output = orm.Required(str)
+        info_hash = orm.Required(str)
+        info = orm.Required(str)
+
+    db.bind(provider='sqlite', filename='~/models/llama_cpp_http.sqlite', create_db=True)
+    db.generate_mapping(create_tables=True)
+    orm.set_sql_debug(True)
+
+
+def build_llama_cpp_cmd(device, prompt, model, n_predict, ctx_size, batch_size, temperature, n_gpu_layers, top_k, top_p):
+    pi, di, p, d = device
+    shell_prompt = shlex.quote(prompt)
+    cmd = []
+
+    if BACKEND == 'cpu':
+        pass
+    elif BACKEND == 'clblast':
+        cmd.extend([
+            f'GGML_OPENCL_PLATFORM={pi}', 
+            f'GGML_OPENCL_DEVICE={di}',
+        ])
+    elif BACKEND == 'clblis':
+        pass
+    else:
+        raise ValueError(BACKEND)
+
+    cmd.extend([
+        f'{LLAMA_CPP_PATH}/main',
+        '--model', f'{MODELS_PATH}/{model}',
+        '--n-predict', n_predict,
+        '--ctx-size', ctx_size,
+        '--batch-size', batch_size,
+        '--temp', temperature,
+        '--n-gpu-layers', n_gpu_layers,
+        '--top-k', top_k,
+        '--top-p', top_p,
+        # '--mlock',
+        # '--no-mmap',
+        '--simple-io',
+        '--prompt', shell_prompt,
+    ])
+
+    cmd = ' '.join(cmd)
+    return cmd
 
 #
 # web server
@@ -85,30 +157,28 @@ async def post_api_1_0_text_completion(request, id_):
     stderr = None
 
     while True:
-        for (pi, di, p, d), lock in cl_devices_locks:
+        for dl in devices_locks:
+            device, lock = dl
+            pi, di, p, d = device
+
             if lock.locked():
                 print(f'platform {pi}, device {di}: locked')
                 continue
             
             print(f'platform {pi}, device {di}: available')
-
-            cmd = ' '.join([
-                f'GGML_OPENCL_PLATFORM={pi}', 
-                f'GGML_OPENCL_DEVICE={di}', 
-                f'{LLAMA_CPP_PATH}/main',
-                '--model', f'{MODELS_PATH}/{model}',
-                '--n-predict', n_predict,
-                '--ctx-size', ctx_size,
-                '--batch-size', batch_size,
-                '--temp', temperature,
-                '--n-gpu-layers', n_gpu_layers,
-                '--top-k', top_k,
-                '--top-p', top_p,
-                '--mlock',
-                '--no-mmap',
-                '--simple-io',
-                '--prompt', shell_prompt,
-            ])
+            
+            cmd = build_llama_cpp_cmd(
+                device=device,
+                prompt=prompt,
+                model=model,
+                n_predict=n_predict,
+                ctx_size=ctx_size,
+                batch_size=batch_size,
+                temperature=temperature,
+                n_gpu_layers=n_gpu_layers,
+                top_k=top_k,
+                top_p=top_p,
+            )
             # print('cmd:', cmd)
 
             async with lock:
@@ -125,11 +195,6 @@ async def post_api_1_0_text_completion(request, id_):
                         stdout, stderr = await proc.communicate()
                         stdout = stdout.decode('unicode-escape')
                         stderr = stderr.decode('unicode-escape')
-                except asyncio.TimeoutError as e:
-                    try:
-                        proc.kill()
-                    except Exception as e:
-                        print('proc kill:', e)
                 except Exception as e:
                     try:
                         proc.kill()
@@ -192,7 +257,10 @@ async def _text_completion_stream(ws, id_, data):
     stderr = None
 
     while True:
-        for (pi, di, p, d), lock in cl_devices_locks:
+        for dl in devices_locks:
+            device, lock = dl
+            pi, di, p, d = device
+
             if lock.locked():
                 print(f'platform {pi}, device {di}: locked')
                 
@@ -208,23 +276,18 @@ async def _text_completion_stream(ws, id_, data):
             
             print(f'platform {pi}, device {di}: available')
 
-            cmd = ' '.join([
-                f'GGML_OPENCL_PLATFORM={pi}', 
-                f'GGML_OPENCL_DEVICE={di}', 
-                f'{LLAMA_CPP_PATH}/main',
-                '--model', f'{MODELS_PATH}/{model}',
-                '--n-predict', n_predict,
-                '--ctx-size', ctx_size,
-                '--batch-size', batch_size,
-                '--temp', temperature,
-                '--n-gpu-layers', n_gpu_layers,
-                '--top-k', top_k,
-                '--top-p', top_p,
-                '--mlock',
-                '--no-mmap',
-                '--simple-io',
-                '--prompt', shell_prompt,
-            ])
+            cmd = build_llama_cpp_cmd(
+                device=device,
+                prompt=prompt,
+                model=model,
+                n_predict=n_predict,
+                ctx_size=ctx_size,
+                batch_size=batch_size,
+                temperature=temperature,
+                n_gpu_layers=n_gpu_layers,
+                top_k=top_k,
+                top_p=top_p,
+            )
             # print('cmd:', cmd)
 
             async with lock:
@@ -267,11 +330,6 @@ async def _text_completion_stream(ws, id_, data):
                         
                         stderr = await proc.stderr.read()
                         stderr = stderr.decode('unicode-escape')
-                except asyncio.TimeoutError as e:
-                    try:
-                        proc.kill()
-                    except Exception as e:
-                        print('proc kill:', e)
                 except Exception as e:
                     try:
                         proc.kill()
@@ -323,18 +381,19 @@ async def get_api_1_0_text_completion(request, id_):
     await ws.prepare(request)
     print('websocket connection opened')
 
-    async for msg in ws:
-        if msg.type == aiohttp.WSMsgType.TEXT:
-            data = json.loads(msg.data)
+    async with asyncio.TaskGroup() as tg:
+        async for msg in ws:
+            if msg.type == aiohttp.WSMsgType.TEXT:
+                data = json.loads(msg.data)
 
-            # run in background
-            coro = _text_completion_stream(ws, id_, data)
-            task = asyncio.create_task(coro)
-        elif msg.type == aiohttp.WSMsgType.ERROR:
-            print(f'ws connection closed with exception {ws.exception()}')
-            break
-        else:
-            print(f'ws msg.type:', msg.type)
+                # run in background
+                coro = _text_completion_stream(ws, id_, data)
+                task = tg.create_task(coro)
+            elif msg.type == aiohttp.WSMsgType.ERROR:
+                print(f'ws connection closed with exception {ws.exception()}')
+                break
+            else:
+                print(f'ws msg.type:', msg.type)
 
     print('websocket connection closed')
     return ws
@@ -362,6 +421,6 @@ def get_app():
 if __name__ == '__main__':
     print(cli_args)
 
-    init_cl_devices()
+    init_devices()
     app = get_app()
     web.run_app(app, host=HOST, port=PORT)
